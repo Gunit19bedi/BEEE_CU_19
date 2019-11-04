c825b0c5ba0c401aa718517aca961571c0a67f0a
Gunit Kumar Bedi <55044594+Gunit19bedi@users.noreply.github.com>
2019-11-04 23:26:43 +0530

    Update CODE.ino

--- EXP8_DIGITAL_DATA_DISPLAY_SYSTEM/CODE.ino
+++ EXP8_DIGITAL_DATA_DISPLAY_SYSTEM/CODE.ino
@@ -1,1 +1,20 @@
 
+//Programmable Digital Data Display System
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  
+  lcd.begin(16, 2);
+
+  lcd.print("EXP 7");
+}
+
+void loop() {
+  
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
+//Name - Gunit Kumar Bedi
+//UID - 19BCS3552
